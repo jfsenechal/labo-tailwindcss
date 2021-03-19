20f2d81294601bc2b20a101bb0da14abf3cc57c0
jfsenechal <jfsenechal@gmail.com>
2021-03-19 21:09:12 +0100

    essais home

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,26 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {
-      backgroundColor: ["active"],
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {}, colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            pink: {
+                light: '#ff7ce5',
+                DEFAULT: '#ff49db',
+                dark: '#ff16d1',
+            },
+            marchebe: {
+                light: '#FFCF1B',
+                DEFAULT: '#4AE0C7',
+                dark: '#487f89',
+            },
+        }
     },
-  },
-  plugins: [],
+    variants: {
+        extend: {
+            backgroundColor: ["active"],
+        },
+    },
+    plugins: [],
 };
